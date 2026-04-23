bbb01fcb6eceb1124a5e9f04423e8b4b1013d144
Pedrito <pedrito@ucc.mx>
2026-04-22 19:54:17 -0600

    Con cookies httpOnly

---
+++ demo_react/DemoSesion/server.js
@@ -0,0 +1,53 @@
+// npm install express cors cookie-parser jsonwebtoken
+// Ejecutar con node server.js desde otra terminal
+
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import jwt from "jsonwebtoken";
+
+import auth from "./src/cookieHttpOnly/auth.js";
+
+const app = express();
+app.use(express.json());
+app.use(cookieParser());
+
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials: true
+}));
+
+const SECRET="supersupersecreto"; // debe ir en un env
+
+app.post("/login", (req, res) => {
+    const { username, password} = req.body;
+
+    //fake validation
+    if (username==="admin" && password==="12345"){
+        const token=jwt.sign({username},SECRET,{expiresIn:"1h"});
+        res.cookie("token", token, {
+            httpOnly: true,
+            secure: false,
+            sameSite: "strict",
+            maxAge: 3600000
+        });
+        return res.json({message:"Login exitoso"})
+    }
+    return res.status(401).json({message:"Credenciales invalidas"});
+});
+
+app.get("/perfil", auth, (req, res)=>{
+    res.json({
+        message:"Eres un usuario protegido",
+        user: req.username
+    });
+});
+
+app.post("/logout", (req, res)=>{
+    res.clearCookie("token");
+    res.json({message: "Logout exitoso"});
+});
+
+app.listen(3000, ()=>{
+    console.log("Servidor en http:localhost:3000");
+});
--- demo_react/DemoSesion/src/main.jsx
+++ demo_react/DemoSesion/src/main.jsx
@@ -3,9 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 //import App from './App.jsx'
 //import AppLocalStorage from './sessionLocalStorage/AppLocalStorage'
-import AppCookie from './cookiesSesion/AppCookie'
+//import AppCookie from './cookiesSesion/AppCookie'
+import AppCookieHttpOnly from './cookieHttpOnly/AppCookieHttpOnly'
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <AppCookie />
+    <AppCookieHttpOnly />
   </StrictMode>,
 )
